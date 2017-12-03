1d8d082b408562b25bb9fda2f94eb8555c7345ff
Vadim Smakhtin <vadim.smakhtin@mathrioshka.ru>
2017-12-03 21:31:31 +0300

    Updated references via NuGet, removed not working Hover node.

--- src/TouchGestureNode.cs
+++ src/TouchGestureNode.cs
@@ -32,34 +32,4 @@ namespace VVVV.Nodes.PatternTouch
 			}
 		}
 	}
-
-    [PluginInfo(Name = "Hover", Category = "PatternTouch", Version = "Gesture", Help = "Detects when you hover the object", Tags = "multitouch")]
-    public class HoverGestureNode : TransformProcessor
-    {
-        [Input("Number Of Fingers", DefaultValue = 1, Visibility = PinVisibility.OnlyInspector)]
-        public ISpread<int> MinFingersIn;
-
-        [Output("Hovered")]
-        public ISpread<bool> HoveredOutput;
-
-        protected override void Reinit(int index)
-        {
-            TransformStates.Add(new TransformState(IdIn[index], new Vector2D()) {Hover = true});
-        }
-
-        protected override void Reset(int index)
-        {
-            TransformStates[index].Reset(IdIn[index], 0);
-        }
-
-        protected override void OutputData(int spreadMax)
-        {
-            HoveredOutput.SliceCount = spreadMax;
-
-            for (var i = 0; i < spreadMax; i++)
-            {
-                HoveredOutput[i] = (TransformStates[i].Phase == TransformPhase.Transforming) && TransformStates[i].Blobs.SliceCount >= MinFingersIn[i];
-            }
-        }
-    }
 }
